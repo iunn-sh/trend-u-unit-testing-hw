350fef23839a7779adbf66651c215d05852b3555
jbytw <bepotw@gmail.com>
2016-02-24 17:07:22 +0800

    codecov

--- src/main/java/homework/week1to4/RandomConstructor.java
+++ src/main/java/homework/week1to4/RandomConstructor.java
@@ -1,19 +1,13 @@
 package homework.week1to4;
 
-import java.util.Random;
-
-
-public class RandomConstructor implements IRandom
-{
+public class RandomConstructor implements IRandom {
 	IRandom random = null;
-	
-	public RandomConstructor(IRandom random)
-	{
+
+	public RandomConstructor(IRandom random) {
 		this.random = random;
 	}
-	
-	public int nextInt(int bound)
-	{
+
+	public int nextInt(int bound) {
 		return random.nextInt(bound);
 	}
 }
